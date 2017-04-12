30237d6698951a8858717c8f5c03b3eeefdda273
Persi <piotrekp.user@gmail.com>
2017-04-12 20:40:07 +0200

    Podstawy:
    MainFrame: glowne okno programu, zawiera w sobie Panel, na ktorym rozgrywana jest gra
    
    MainPanel: glowny panel rozgrywki, posiada informacje o Kulce oraz o Paletce
    
    Ball:
     • ruch po wektorach: [-1,-1], [-1,1], [1,-1], [1,1]
     • odbijanie od scian
    
    Paddle: narazie to tylko pojemnik na dane dotyczace paletki.

--- src/objects2D/Puck.java
+++ src/objects2D/Puck.java
@@ -1,5 +1,18 @@
 package objects2D;
 
-public class Puck
+public class Puck extends Shape
 {
+    public static double radius = 10;
+
+    Puck()
+    {
+        this.width = radius;
+        this.height = radius;
+    }
+
+    @Override
+    public Shape getGraphics()
+    {
+        return this;
+    }
 }

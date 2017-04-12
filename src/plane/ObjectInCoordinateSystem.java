2f8b6bacbd2e9e0a7320221dd895f2ef73ef2c40
Persi <piotrekp.user@gmail.com>
2017-04-12 23:37:53 +0200

    getery dla klasy ObjectInCoordinateSystem

--- src/plane/ObjectInCoordinateSystem.java
+++ src/plane/ObjectInCoordinateSystem.java
@@ -3,11 +3,17 @@ package plane;
 public class ObjectInCoordinateSystem implements Moveable
 {
     private Sprite sprite;
+    private Point point;
 
     ObjectInCoordinateSystem(Sprite sprite)
     {
         this.sprite = sprite;
+        this.point = new Point();
     }
+
+    Point getPoint(){ return this.point; }
+    double getWidth(){ return this.sprite.shape.width; }
+    double getHeight(){ return this.sprite.shape.height; }
 
     @Override
     public double getVelocity()
